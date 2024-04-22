02c25b9bb62ee29628e2921d56115b875c6408f7
hossam ameen <hossam.ameen@shipblu.com>
2024-04-22 12:19:15 +0200

    add python version 3.10

--- activities/urls.py
+++
@@ -1,8 +0,0 @@
-from activities.api import LectureViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-
-router.register(r'lectures', LectureViewSet)
-
-urlpatterns = router.urls
